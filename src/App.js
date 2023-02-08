51431cf67d27a0c40f3d592a9e21fb06259887d5
Romina Trazzi <romina.trazzi@posta.it>
2023-02-08 18:40:46 +0100

    Aggiunto css inline

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 function App() {
   const title ="Welcome to the new blog";
   const like = Math.floor(Math.random() * 100);
+  const link = "https://www.Google.it";
+
   return (
+  
     <div className="App">
       <div className="content">
+
         <h1>{ title }</h1>
         <p> Liked { like } times </p>
+        
+        <a href = { link } style = {{ color: "black", backgroundColor: "yellow" }} > Google Site </a>
+
+      
       </div>
     </div>
   );
