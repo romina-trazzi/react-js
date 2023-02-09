0a3eab85ae9f3ad1ad30080626e2cfa562e62ead
Romina Trazzi <romina.trazzi@posta.it>
2023-02-09 04:22:21 +0100

    Aggiunta Use state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [name, setName] = useState("");
   const title ="Welcome to the new blog";
   const like = Math.floor(Math.random() * 100);
   const link = "https://www.Google.it";
 
   const handleClick = () => {
-    console.log("Hello!");
+    setName("");
   }
 
-  const handleClickAgain = (name) => {
-    console.log(`Hello ${name}`);
+  const handleClickAgain = (oldName) => {
+    setName(oldName);
+    console.log(`Hello ${oldName}`);
   }
 
+  
   return (
   
     <div className="App">
       <div className="content">
 
-        <h1>{ title }</h1>
+        <h1>{  title } </h1>
         <p> Liked { like } times </p>
         
         <a href = { link } style = {{ color: "black", backgroundColor: "yellow" }} > Google Site </a>
@@ -33,13 +37,19 @@ function App() {
             marginTop:"50px" }}> Click me 
           </button>
 
-          <button onClick = { () => { handleClickAgain('Romy') } } style = {{ 
+          <button onClick = { () => { handleClickAgain('Romy') }} style = {{ 
             position: "absolute", 
             top:"50%", 
             left:"50%", 
             transform: "translate(-50%, -50%)", 
             marginTop:"100px" }}> Click me again
           </button>
+          
+          {/* condizione ? true : false */}
+          {/* { name === "" ? null : <p> Old name { name } </p> } */}
+
+          { name && <p> Old name { name } </p> }
+
         </div>
 
       
