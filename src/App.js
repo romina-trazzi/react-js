1387d2bb9cb8f009fd92e59f1de048cf9861f542
Romina Trazzi <romina.trazzi@posta.it>
2023-02-09 06:14:19 +0100

    Aggiunta new component e props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import './blogList';
+import BlogList from './blogList';
 
 function App() {
   
+  // UseState variables
   const [name, setName] = useState("");
+  const [blogs, setBlog] = useState([
+
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+
+  ]);
+  
+
+  // Non reactive variables
   const title ="Welcome to the new blog";
   const like = Math.floor(Math.random() * 100);
   const link = "https://www.Google.it";
 
+
+  // Functions 
   const handleClick = () => {
     setName("");
   }
 
   const handleClickAgain = (oldName) => {
+    oldName = prompt("Caro utente inserisci un nome");
     setName(oldName);
-    console.log(`Hello ${oldName}`);
   }
 
   
@@ -34,21 +49,28 @@ function App() {
             top:"50%", 
             left:"50%", 
             transform: "translate(-50%, -50%)", 
-            marginTop:"50px" }}> Click me 
+            marginTop:"200px" }}> Click me 
           </button>
 
-          <button onClick = { () => { handleClickAgain('Romy') }} style = {{ 
+          <button onClick = { () => { handleClickAgain('') }} style = {{ 
             position: "absolute", 
             top:"50%", 
             left:"50%", 
             transform: "translate(-50%, -50%)", 
-            marginTop:"100px" }}> Click me again
+            marginTop:"300px" }}> Click me again
           </button>
           
           {/* condizione ? true : false */}
           {/* { name === "" ? null : <p> Old name { name } </p> } */}
 
+          {/* Rendering conditional */}
           { name && <p> Old name { name } </p> }
+
+          <div className="home"> 
+
+            <BlogList blogs={ blogs }> </BlogList>
+          
+          </div>
 
         </div>
 
---
+++ src/blogList.js
@@ -0,0 +1,30 @@
+const BlogList = (props) => {
+    
+    // Importiamo dal componente padre APP la lista dei blog
+    const blogs = props.blogs;
+    
+    
+    return (  
+
+        <div className="blog_list">
+
+            <h2> Cicling into the blogs </h2>
+
+            {/* Creiamo il template ciclando nell'array dei blogs */}
+            
+            { blogs.map((blog) => (
+                
+                <div key={blog.id}> 
+                    <h3> {blog.title} </h3>
+                    <p> Written by {blog.author} </p>
+                </div>
+
+            ))}
+
+        </div>
+
+    );
+     
+}
+ 
+export default BlogList;
